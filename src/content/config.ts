f1bac7a6e8ce4b88d9e9e4b8386556b573ff8c30
hel-kun <farflylonglight@gmail.com>
2023-12-12 22:38:54 +0900

    add new website

---
+++ src/content/config.ts
@@ -0,0 +1,14 @@
+import { defineCollection, z } from 'astro:content';
+
+const blog = defineCollection({
+	// Type-check frontmatter using a schema
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		// Transform string to Date object
+		date: z.coerce.date(),
+		heroImage: z.string().optional(),
+	}),
+});
+
+export const collections = { blog };
---
+++ tailwind.config.js
@@ -0,0 +1,42 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{astro,js,ts}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        footergray: {
+          light: '#60739f',
+          DEFAULT: '#60739f',
+          dark: '#c5c9d4',
+        },
+        footerhovergray: {
+          light: '#222939',
+          DEFAULT: '#222939',
+          dark: '#f5f7fc',
+        },
+      },
+      screens: {
+        x3lmax: { max: "2550px" },
+        // => @media (max-width: 1535px) { ... }
+        
+        x2lmax: { max: "1650px" },
+        // => @media (max-width: 1279px) { ... }
+        
+        xlmax: { max: "1279px" },
+        // => @media (max-width: 1279px) { ... }
+        
+        lgmax: { max: "1023px" },
+        // => @media (max-width: 1023px) { ... }
+        
+        mdmax: { max: "767px" },
+        // => @media (max-width: 767px) { ... }
+        
+        smax: { max: "639px" },
+        // => @media (max-width: 639px) { ... }
+      },
+    },
+  },
+  plugins: [],
+}
