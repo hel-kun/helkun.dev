51ff2f87ad994288d07af52828ad976b66e233be
hel-kun <farflylonglight@gmail.com>
2024-04-22 01:01:51 +0900

    ブログの目次の基礎部分の実装

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,22 @@ import tailwind from "@astrojs/tailwind";
 import AutoImport from "astro-auto-import";
 import react from "@astrojs/react";
 import expressiveCode from "astro-expressive-code";
-const mdxIntegrations = [AutoImport({
-  imports: ["./src/components/card/UrlCard.astro", "./src/components/card/TwitterCard.astro", "./src/components/card/ImgCard.astro", "./src/components/ui/Blockquote.astro", "./src/components/box/Box.astro", "./src/components/box/WarningBox.astro", "./src/components/ui/YoutubePlayer.astro"]
-}), mdx()];
+import rehypeToc from 'rehype-toc';
+import { css } from '@emotion/react';
+const mdxIntegrations = [
+  AutoImport({
+    imports: [
+      "./src/components/card/UrlCard.astro",
+      "./src/components/card/TwitterCard.astro",
+      "./src/components/card/ImgCard.astro",
+      "./src/components/ui/Blockquote.astro",
+      "./src/components/box/Box.astro",
+      "./src/components/box/WarningBox.astro",
+      "./src/components/ui/YoutubePlayer.astro"
+    ]
+  }), 
+  mdx(),
+];
 
 
 // https://astro.build/config
@@ -17,4 +30,7 @@ export default defineConfig({
     expressiveCode(),
     ...mdxIntegrations,
   ],
+  markdown: {
+    rehypePlugins: [[rehypeToc, { headings: ['h2', 'h3'], cssClasses: { toc: 'toc' }}]],
+  },
 });
