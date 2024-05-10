59f42063e66ab1a8bca04247e48c66a569e0fa37
hel-kun <farflylonglight@gmail.com>
2024-05-11 01:00:16 +0900

    webルーレット制作開始

---
+++ src/components/app/Roulette.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="wheel">
+    </div>
+    <button>Spin</button>
+
+    <input placeholder="新しい項目" />
+
+    <button>追加</button>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+</style>
+
+<!--
+  
+-->
