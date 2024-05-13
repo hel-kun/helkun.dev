d8c0b0ce8e32c16ab36d5d5570d6677ed9427ea2
hel-kun <farflylonglight@gmail.com>
2024-05-13 17:25:58 +0900

    webルーレットのベータ版の制作

---
+++ src/components/app/Roulette/PopupCard.vue
@@ -0,0 +1,47 @@
+<template>
+    <div v-if="visible" class="popup">
+      <div class="content" >
+        <p>選ばれたのは...</p>
+        <h2 class="message">{{ message }}</h2>
+        <button class="closeButton" @click="hide">閉じる</button>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        message: String,
+    },
+    data() {
+        return {
+            message: '',
+            visible: false,
+        };
+    },
+    methods: {
+        show(message) {
+            this.message = message;
+            this.visible = true;
+        },
+        hide() {
+            this.visible = false;
+        }
+    }
+};
+</script>
+
+<style scoped>
+.popup {
+    @apply fixed top-0 left-0 w-full h-full bg-black dark:bg-white bg-opacity-50 dark:bg-opacity-50 flex justify-center items-center z-50;
+    .content {
+        @apply bg-white dark:bg-gray-800 p-4 rounded-md sm:w-1/3 w-2/3 text-black dark:text-white;
+        .message{
+            @apply text-center m-2 sm:m-4;
+        }
+        .closeButton {
+            @apply flex items-center bg-red-500 text-white p-2 rounded-md hover:bg-red-700 mx-auto my-1;
+        }
+    }
+}
+</style>
---
+++ src/components/app/Roulette/Roulette.vue
@@ -0,0 +1,110 @@
+<template>
+    <h1 class="font-bold text-4xl mb-4">Webルーレット(beta版)</h1>
+    <p class="mb-4">ルーレットを回してみよう！</p>
+<!--
+    <div class="m-4">
+        <canvas ref="canvas" width="500" height="500"></canvas>
+    </div>
+-->
+    <PopupCard ref="popup" />
+
+    <div class="tableTitle">
+        <p>抽選項目</p>
+        <p>当選比率</p>
+    </div>
+    <form class="label">
+        <ul>
+            <li v-for="(entry, index) in items" :key="index">
+                <input class="text-black" v-model="entry.label" type="text" placeholder="項目内容" />
+                <input class="text-black" v-model.number="entry.rate" type="number" placeholder="比率" />
+                <button class="deleteButton" type="button" @click="removeItem(index)" >削除</button>
+            </li>
+        </ul>
+    </form>
+    
+    <button class="addButton" type="button" @click="addItem" >項目追加</button>
+    <button class="spinButton" type="button" @click="spin" >抽選を回す</button>
+</template>
+
+<script>
+import PopupCard from '@/components/app/Roulette/PopupCard.vue';
+export default {
+    components: {
+        PopupCard
+    },
+    data() {
+        return {
+            items: [{ label: '', rate: 1 }]
+        };
+    },
+    //mounted() {
+    //    this.setupCanvas();
+    //},
+    methods: {
+        addItem() {
+            console.log('Add item!');
+            this.items.push({ label: '', rate: 1 });
+        },
+        removeItem(index) {
+            if(this.items.length === 1){
+                window.alert('最後の項目は削除できません。');
+            }else{
+                console.log('Remove item!');
+                this.items.splice(index, 1);
+            }
+        },
+        spin() {
+            console.log('Spin the wheel!');
+            let totalRate=0;
+            for (let item of this.items) {
+                totalRate += item.rate;
+            }
+            let random = Math.random() * totalRate;
+            let sum = 0;
+
+            for (let item of this.items) {
+                sum += item.rate;
+                if (random < sum) {
+                    console.log('Selected:', item.label);
+                    this.$refs.popup.show(item.label);
+                    break;
+                }
+            }
+        },
+        //setupCanvas() {
+        //    const canvas = this.$refs.canvas;
+        //    const ctx = canvas.getContext('2d');
+        //    ctx.fillStyle = 'rgb(255, 0, 0)';
+        //    ctx.arc(100, 100, 50, 0, Math.PI * 2, false);
+        //    ctx.fill();
+        //}
+    },
+}
+</script>
+
+<style scoped>
+.tableTitle{
+    @apply flex flex-nowrap;
+    p{
+        @apply font-bold text-center mx-1 px-1 w-[37.5%] max-w-52;
+    }
+}
+ul {
+    @apply list-none p-0;
+    li{
+        @apply flex flex-nowrap h-12 m-2;
+        input {
+            @apply m-1 p-1 w-[37.5%] max-w-52 rounded-md border border-gray-300;
+        }
+        .deleteButton {
+            @apply bg-red-500 text-white m-1 p-2 rounded-md hover:bg-red-700;
+        }
+    }
+}
+.spinButton {
+    @apply bg-blue-500 text-white m-2 p-2 rounded-md hover:bg-blue-700;
+}
+.addButton {
+    @apply bg-green-500 text-white m-2 p-2 rounded-md hover:bg-green-700;
+}
+</style>
