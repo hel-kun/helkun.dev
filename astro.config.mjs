09e2982dd2aa2d79d864d20bca673938b9097f9b
HEL <114824440+hel-kun@users.noreply.github.com>
2026-05-29 14:44:50 +0900

    制作物の更新 (#67)
    * いくつかのパッケージのバージョンの更新とそれに伴う依存関係の修正
    * 制作物の追加
    * スキーマの修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
-import tailwind from "@astrojs/tailwind";
 import AutoImport from "astro-auto-import";
 import react from "@astrojs/react";
 import expressiveCode from "astro-expressive-code";
 import rehypeSlug from "rehype-slug";
-import { css } from '@emotion/react';
 import vue from "@astrojs/vue";
 const mdxIntegrations = [AutoImport({
   imports: ["./src/components/card/UrlCard.astro", "./src/components/card/TwitterCard.astro", "./src/components/card/ImgCard.astro", "./src/components/ui/Blockquote.astro", "./src/components/box/Box.astro", "./src/components/box/WarningBox.astro", "./src/components/ui/YoutubePlayer.astro"]
@@ -14,7 +12,7 @@ const mdxIntegrations = [AutoImport({
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), expressiveCode(), ...mdxIntegrations, vue()],
+  integrations: [react(), expressiveCode(), ...mdxIntegrations, vue()],
   markdown: {
     rehypePlugins: [rehypeSlug],
   }
--- src/content.config.ts
+++ src/content.config.ts
@@ -1,5 +1,6 @@
 import { glob } from 'astro/loaders';
-import { defineCollection, z } from 'astro:content';
+import { defineCollection } from 'astro:content';
+import { z } from 'astro/zod';
 
 const blog = defineCollection({
   loader: glob({base: './src/content/blog', pattern: '**/[^_]*.{md,mdx}'}),
@@ -10,17 +11,28 @@ const blog = defineCollection({
     date: z.date(),
     heroImage: z.string(),
 		published: z.boolean(),
+    updated: z.date().optional(),
   }),
 });
 
 const links = defineCollection({
-  loader: glob({base: './src/content/links', pattern: '**/*.{json}'}),
+  loader: glob({base: './src/content/links', pattern: '**/*.json'}),
   schema: z.any(),
 });
 
 const works = defineCollection({
-  loader: glob({base: './src/content/works', pattern: '**/*.{json}'}),
-  schema: z.any(),
+  loader: glob({base: './src/content/works', pattern: '**/*.json'}),
+  schema: z.array(
+    z.object({
+      title: z.string().min(1),
+      image: z.url(),
+      date: z.string().regex(/^\d{4}-\d{2}$/, 'Must be formatted as YYYY-MM'),
+      endDate: z.union([z.string().regex(/^\d{4}-\d{2}$/, 'Must be formatted as YYYY-MM'), z.literal('now')]).optional(),
+      description: z.string().min(1),
+      github: z.url().optional(),
+      siteUrl: z.url().optional(),
+    }),
+  ),
 });
 
-export const collections = { blog };
+export const collections = { blog, links, works };
