39a60b251022dfc5dbd6faf406e2961a295e035c
helkun <farflylonglight@gmail.com>
2026-05-29 14:23:16 +0900

    パッケージを依存関係の修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 import AutoImport from "astro-auto-import";
 import react from "@astrojs/react";
 import expressiveCode from "astro-expressive-code";
 import rehypeSlug from "rehype-slug";
-import { css } from '@emotion/react';
 import vue from "@astrojs/vue";
 const mdxIntegrations = [AutoImport({
   imports: ["./src/components/card/UrlCard.astro", "./src/components/card/TwitterCard.astro", "./src/components/card/ImgCard.astro", "./src/components/ui/Blockquote.astro", "./src/components/box/Box.astro", "./src/components/box/WarningBox.astro", "./src/components/ui/YoutubePlayer.astro"]
@@ -14,7 +13,10 @@ const mdxIntegrations = [AutoImport({
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), expressiveCode(), ...mdxIntegrations, vue()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  integrations: [react(), expressiveCode(), ...mdxIntegrations, vue()],
   markdown: {
     rehypePlugins: [rehypeSlug],
   }
